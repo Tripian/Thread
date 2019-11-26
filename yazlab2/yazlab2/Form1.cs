f43ff07499e73ab9eea925ebb474d7492df58e40
Tripian <aes.97.aes@gmail.com>
2019-11-26 23:06:11 +0300

    Thread

---
+++ yazlab2/yazlab2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace yazlab2
+{
+
+    public partial class Form1 : Form
+    {
+        int j = 10, i = 0;
+
+        MainThread mt = new MainThread();
+        Thread mt1 = new Thread(new ThreadStart(mt.requestAccept));
+        Thread mt2 = new Thread(new ThreadStart(mt.requestReturn));
+        
+        /*List<Thread> subthreads = new List<Thread>(new ThreadStart(work2));
+        Thread threadcreator = new Thread(new ThreadStart(work3));
+        Thread threadwatcher = new Thread(new ThreadStart(work4));*/
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+            progressBar1.Increment(j);
+            i += 10;
+            if (i >= 70)
+            {
+                i = i / 2;
+                progressBar1.Value = i;
+            }
+        }
+    }
+}
---
+++ yazlab2/yazlab2/Thread.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace yazlab2
+{
+    public class MainThread
+    {
+        public int request = 0;
+        public int capacity = 10000;
+
+        public void requestAccept()
+        {
+            Random rnd = new Random();
+            request += rnd.Next(1, 101);
+            if (request > capacity)
+            {
+                request = capacity;
+            }
+            Thread.Sleep(500);
+        }
+
+        public void requestReturn()
+        {
+            Random rnd = new Random();
+            request -= rnd.Next(1, 51);
+            if (request < 0)
+            {
+                request = 0;
+            }
+            Thread.Sleep(200);
+        }
+    }
+}
